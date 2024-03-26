ede3c313617d3e96e43ce79b7238cc4219398496
thespbgamer <30112696+thespbgamer@users.noreply.github.com>
2024-03-26 21:08:33 +0000

    Initial version

---
+++ FolderSynchronization/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length < 4)
+        {
+            Console.WriteLine("Usage: program.exe <source_folder> <replica_folder> <sync_interval_seconds> <log_file_path>");
+            return;
+        }
+    }
+}
